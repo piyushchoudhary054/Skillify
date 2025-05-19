202bd4fcb91bdacc8fb434a015ad6ab3e06d21cf
piyushchoudhary054 <piyushchoudhary054@gmail.com>
2025-05-19 12:00:02 +0530

    changes in orignis

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 // ✅ Allowed CORS origins
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://adorable-brioche-cfd86e.netlify.app'
+  'skillify-theta.vercel.app'
 ];
 
 // ✅ CORS middleware — only once and BEFORE all routes
